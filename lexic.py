9b43ef7d2204c122105a3f759cfdb8eaf83bfaf8
peth <pethersonreis@gmail.com>
2025-03-11 12:35:32 -0300

    fix: nome de variáveis e imports

--- lexic.py
+++ lexic.py
@@ -1,9 +1,11 @@
 import re
 import sys
 
+from token_class import Token
+
 
 class AnalizadorSintatico:
-    TOKENS = {
+    REGEX = {
         'palavras_reservadas': r'\b(var|if|while|F|V)\b',  # \b é para garantir que sejam palavras inteiras
         'numero': r'\b\d+(\.\d+)?\b',  # números inteiros e flutuantes
         'variavel': r'[a-zA-Z_]\w*',
@@ -14,23 +16,24 @@ class AnalizadorSintatico:
         'operador_relacionais': r'==|<>|>|<|>=|<='
     }
 
+    tokens = []
+
     def __init__(self, code):
         self.code = code
 
-    def structure_validation(self):
+    def tokenizar(self):
         linhas = self.code.strip().split("\n")
 
         def check(i, linha_to_check, linha_index):
             if len(linha_to_check) == i:
                 return
 
-
             comando = linha_to_check[i]
-            for tipo_token, regex in self.TOKENS.items():
+            for tipo_token, regex in self.REGEX.items():
                 padrao = re.compile(regex)
                 resultado = padrao.fullmatch(comando)
                 if resultado is not None:
-                    print(tipo_token + ": " + comando)
+                    self.tokens.append(Token(tipo_token, comando))
                     return check(i + 1, linha_to_check, linha_index)
             print(f"Error de sintaxe na linha: {linha_index + 1}", file=sys.stderr)
             exit()
@@ -39,3 +42,6 @@ class AnalizadorSintatico:
             linha_splitada = [l for l in linha.split(" ") if l != ""]
 
             check(0, linha_splitada, index)
+
+    def get_token(self):
+        return [tk.__str__() for tk in self.tokens]
--- main.py
+++ main.py
@@ -3,14 +3,15 @@ from lexic import AnalizadorSintatico
 code = """
 var x = 10 + 3.14
 if x > 5
-6d das
 while x < 20 AND x < 1 0
 """
 
 
 def main():
     analizador_sintatico = AnalizadorSintatico(code)
-    analizador_sintatico.structure_validation()
+    analizador_sintatico.tokenizar()
+
+    print(analizador_sintatico.get_token())
 
 
 if __name__ == '__main__':
